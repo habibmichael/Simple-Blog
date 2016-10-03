e8aadd0aab4f30a40dada80d286a8034d97c54e8
Michael Habib <michaelhabib1868@gmail.com>
2016-10-03 11:17:47 -0700

    Create migrations for post model, create custom interface for admin site

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+
+
+#Creates site based on Post class in models
+
+#Information on how to display model information
+class PostAdmin(admin.ModelAdmin):
+    list_display=('title','slug','author','publish',
+                  'status')
+    list_filter = ('status','created','publish','author')
+    search_fields = ('title','body')
+    prepopulated_fields = {'slug':('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ['status','publish']
+
+admin.site.register(Post,PostAdmin)
+
+
