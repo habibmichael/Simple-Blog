83f8977ac55245eb7f78f7c1bd0f7743d767949a
Michael Habib <michaelhabib1868@gmail.com>
2016-10-05 10:22:41 -0700

    Create sharing by email functionality & template

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator,EmptyPage,\
     PageNotAnInteger
 from django.views.generic import ListView
-
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 from .models import Post
 
 # Create your views here.
@@ -40,6 +41,33 @@ def post_detail(request,year,month,day,post):
     return render(request,
                   'blog/post/detail.html',
                   {'post':post})
+
+#View for form
+def post_share(request,post_id):
+    #retrieve post by id
+    post = get_object_or_404(Post,id=post_id,status='published')
+    sent = False
+
+    if request.method == 'Post':
+        #Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            #Form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = '{}({}) recommends you reading "{}" '.format(
+                cd['name'],cd['email'],post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(
+                post.title,post_url,cd['name'],cd['comments'])
+            send_mail(subject,message,'admin@myblog.com',
+                      [cd['to']])
+            sent = True
+        # ... send email
+    else:
+        form = EmailPostForm()
+        return render(request,'blog/post/share.html',{'post':post,
+            'form':form,'sent':sent})
 
 
 
